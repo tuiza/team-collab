045f426e246c184ee874e5a1e0e6f745fa566a97
tuiza <luiza8.marlene@gmail.com>
2023-10-30 11:20:29 -0300

    fix: erro de tipagem

--- src/app/private/colaboradores/novoColaborador/schema.ts
+++ src/app/private/colaboradores/novoColaborador/schema.ts
@@ -1,9 +1,7 @@
 import * as z from "zod";
 
-const schemaLogin = z
-  .object({
-    nome: z
-      .string()
+const schemaLogin = z.object({
+    nome: z.string()
       .min(1, "Nome é obrigatório")
       .transform((nome) => {
         return nome
@@ -14,14 +12,14 @@ const schemaLogin = z
           })
           .join(" ");
       }),
-    areas: z.string(),
-    idade: z.number().min(15, "Por favor, Idade inválida"),
-    regimeContratacao: z.string().min(1, "Por favor, Selecione uma opção"),
+    areas: z.array(z.string().min(1, "Selecione uma área")),
+    idade: z.number().min(15, "Idade inválida"),
+    regimeContratacao: z.string().min(1, "Selecione uma opção"),
     email: z.string().email("Email inválido"),
   })
   .refine((data) => data.regimeContratacao.length, {
     path: ["regimeContratacao"],
-    message: "Por favor, Selecione uma opção",
+    message: "Selecione uma opção",
   });
 
 export default schemaLogin;
